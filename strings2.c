9768b654efe41abc88195a0f35bae0aa525ed21f
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-07-13 12:17:33 -0700

    removed printf statement

--- strings2.c
+++ strings2.c
@@ -10,10 +10,8 @@ int _strncmp(char *s1, char *s2, int n)
         int i;
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0' && i < n; i++)
         {
-                if (s1[i] != s2[i])
-		{
-                        return (-1);
-                }
+                if (s1[i] == s2[i])
+                        return (0);
 	}
-	return (0);
+	return (-1);
 }
